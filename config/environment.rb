fc3592279c5960154295807b79116edf94996781
joemcpartland@gmail.com <josephmcpartland@JM.hsd1.co.comcast.net>
2021-03-20 17:15:22 -0600

    Saturday 2 commit

--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,6 @@ require 'net/http'
 require 'json'
 
 require_relative "../models/cli.rb"
+require_relative "../models/nhl_api.rb"
 require_relative "../models/team.rb"
 require_relative "../models/team_search.rb"
